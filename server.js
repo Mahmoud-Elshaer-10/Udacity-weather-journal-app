5eeb91d2cdfcd83424f68f474ef3303b21f38f64
Mahmoud-Elshaer-10 <mahmoud_shaeer@yahoo.com>
2022-02-01 23:10:41 +0200

    up

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 // Start up an instance of app
+const app = express();
+const port = 5000;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,11 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
+
+app.get("/getRoute", (req, res) => {
+  res.send(projectData);
+});
 
 // Setup empty JS object to act as endpoint for all routes
 // Express to run server and routes
--- website/app.js
+++ website/app.js
@@ -1,10 +1,20 @@
 /* Global Variables */
+const btn = document.getElementById("generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
+const apiKey = "6fd48113cf33e8f31b3467f9acd5a9b6";
+
+btn.onclick = async () => {
+  const zipCode = document.getElementById("zip").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const temp = data.main.temp;
+};
 
 // Event listener to add function to existing HTML DOM element
 
