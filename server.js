55ef184cab862fc2fcea90e3d1dc9c381f0000c1
Mahmoud-Elshaer-10 <mahmoud_shaeer@yahoo.com>
2022-02-02 22:03:03 +0200

    up

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Require all dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // Start up an instance of app
 const app = express();
+// declaring port
 const port = 5000;
 
 /* Middleware*/
@@ -24,11 +25,14 @@ app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
 
+// defining POST route endpoint
 app.post("/postRoute", (req, res) => {
-  projectData = { ...req.body };
-  res.send();
+  // saving data received from client-side
+  projectData = req.body;
+  res.end();
 });
 
+// defining GET route endpoint
 app.get("/getRoute", (req, res) => {
   res.send(projectData);
 });
--- website/app.js
+++ website/app.js
@@ -6,19 +6,27 @@ const btn = document.getElementById("generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
+// adding 1 to getMonth because it starts from 0
 let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
 const apiKey = "6fd48113cf33e8f31b3467f9acd5a9b6";
 
+// on clicking generate button function
 btn.addEventListener("click", async () => {
   try {
+    // getting the value of feelings textarea
     const feelings = document.getElementById("feelings").value;
+    // value of zip code entered
     const zipCode = document.getElementById("zip").value;
+    // URL to use to get data from openweathermap.org
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+    // data to be received from external api
     const res = await (await fetch(url)).json();
+    // setting temperature received to temp variable
     const temp = res.main.temp;
 
+    // sending data received from external api to local server using POST method
     const postData = await fetch("/postRoute", {
       method: "POST",
       headers: {
@@ -29,7 +37,10 @@ btn.addEventListener("click", async () => {
       }),
     });
 
+    // getting data from local server using GET method which is the default
     const getData = await (await fetch("/getRoute")).json();
+
+    // updating UI
     dateDiv.innerHTML = `Date is ${newDate}`;
     tempDiv.innerHTML = `Temperature is: ${getData.temp} celsius`;
     contentDiv.innerHTML = `Feelings: ${feelings}`;
@@ -37,13 +48,3 @@ btn.addEventListener("click", async () => {
     console.log(error);
   }
 });
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
-
-/* Function to GET Web API Data*/
-
-/* Function to POST data */
-
-/* Function to GET Project Data */
